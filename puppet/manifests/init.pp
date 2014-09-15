14653dfb9bf8c30ed2380fcaaa91e14ada8e6eb7
Dwi Sasongko S <ruckuus@gmail.com>
2014-09-15 22:03:06 +0800

    Add env param.

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -19,5 +19,6 @@ class { 'puppetry':
   listen_url => '127.0.0.1:9000',
   app_name => 'foliogalery',
   source => 'http://www.foliopages.com/downloads/foliogallery-master-1.3.zip',
+  env => 'dev'
 }
 
--- puppet/modules/puppetry/manifests/init.pp
+++ puppet/modules/puppetry/manifests/init.pp
@@ -3,8 +3,17 @@ class puppetry(
   $document_root = '/var/www',
   $listen_url = '127.0.0.1:9000',
   $app_name = 'puppetry',
-  $source = 'www.google.com/foo'
+  $source = 'www.google.com/foo',
+  $env = 'dev',
+  $tld = 'com'
 ) {
+
+  if $env == 'prod' {
+    $server_names = "www.${app_name}.${tld} ${app_name}.${tld}"
+  } else {
+    $server_names = "${env}.${app_name}.${tld}"
+  }
+
   # Setup nginx
   file { ['/etc/nginx/sites-available', '/etc/nginx/sites-enabled']:
     ensure => directory,
