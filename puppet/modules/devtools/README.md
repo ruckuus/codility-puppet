e3a6cf697f18c106960e4da9bd0b5dfa6b7a3796
Dwi Sasongko S <ruckuus@gmail.com>
2014-09-26 18:31:00 +0800

    Use devtools.

---
+++ puppet/modules/devtools/README.md
@@ -0,0 +1,12 @@
+# devtools
+
+## Overview
+
+A very simple puppet module to help common tools installation.
+
+## Usage
+
+```
+  class { 'devtools': }
+```
+
---
+++ puppet/modules/devtools/manifests/init.pp
@@ -0,0 +1,21 @@
+# == Class: devtools
+#
+# A module that will install common utilities for sysadmin/developer in *nix.
+#
+# === Examples
+#
+#  class { 'devtools': }
+#
+# === Authors
+#
+# Author Name <ruckuus@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2014 Dwi Sasongko S, unless otherwise noted.
+#
+class devtools {
+  package { ['git', 'mtr', 'vim', 'zip', 'htop', 'make', 'iotop', 'iperf', 'ngrep', 'unzip', 'screen', 'strace', 'tcpdump', 'sysstat', 'iproute', 'libpcre3', 'dos2unix', 'iptables', 'bind9utils', 'traceroute', 'libpcre3-dev', 'ntop', 'rsync', 'tig', 'vnstat', 'iptraf', 'curl', 'wget' ]:
+    ensure => latest
+  }
+}
